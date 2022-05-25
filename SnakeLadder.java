09aa4dd1d7583a38bfc282abc11f5ca2abb2c15f
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-26 05:14:09 +0530

    uc4 ruunig program till wining postion 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,10 +1,40 @@
-//welcome message of snake ladder game   
+//Repeat till the Player reaches the winning position 100.
 
 package com.bridgelabz;
+
+import java.util.Random;
+
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator");
-
         System.out.println("The player starts from position zero");
+
+        Random rnum = new Random();
+        int sum = 0, position = 0, options, diceRoll = 0;
+
+        while (sum < 101) {
+
+            options = rnum.nextInt(3);
+            diceRoll = rnum.nextInt(6) + 1;
+
+            switch (options) {
+
+                case 0:
+                    position = 0;
+                    break;
+                case 1:
+                    position = -diceRoll;
+                    break;
+                case 2:
+                    position = diceRoll;
+                    break;
+
+            }
+            sum += position;
+
+            if (sum <= 0)
+                sum = 0;
+        }
+        System.out.println("Player has reached winning position ");
     }
 }
