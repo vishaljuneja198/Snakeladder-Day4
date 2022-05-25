3a895bf927b31ddc032d10baac5a896568434135
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-26 05:04:22 +0530

    uc2 position of player

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,10 +1,26 @@
-//welcome message of snake ladder game   
-
+//The Player then checks for a Option. They are No Play, Ladder or Snake.
 package com.bridgelabz;
+
+import java.util.Random;
+
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator");
-
         System.out.println("The player starts from position zero");
+
+        Random rnum = new Random();
+        int diceRoll = rnum.nextInt(6) + 1;
+
+        int options = rnum.nextInt(3);
+        switch (options) {
+            case 0:
+                System.out.println("Player will stay on the position");
+                break;
+            case 1:
+                System.out.println("Player moves ahead with : " + diceRoll + " number");
+                break;
+            default:
+                System.out.println("Player moves behind : " + diceRoll + " number");
+        }
     }
 }
