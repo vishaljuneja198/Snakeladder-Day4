5c7c3bc6cc7a05e811e617e74ce1a50784638816
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-26 04:59:07 +0530

    uc1 rolling of die

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,10 +1,14 @@
-//welcome message of snake ladder game   
+//The Player rolls the die to get a number between 1 to 6.
 
 package com.bridgelabz;
+import java.util.Random;
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator");
-
         System.out.println("The player starts from position zero");
+
+        Random rnum = new Random();
+        int diceRoll = rnum.nextInt(6)+1;
+        System.out.println("Number on dice is :"+diceRoll);
     }
 }
