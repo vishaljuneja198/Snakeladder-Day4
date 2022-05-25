27f02228402697b6db301b22002751f6c7908a57
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-26 04:55:25 +0530

    welocme message of game

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+//welcome message of snake ladder game   
+
+package com.bridgelabz;
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Simulator");
+
+        System.out.println("The player starts from position zero");
+    }
+}
