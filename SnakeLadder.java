ab490dc895e0c521a668115dce582dfee00455cd
vishaljuneja198 <103586890+vishaljuneja198@users.noreply.github.com>
2022-05-26 22:34:54 +0530

    Update SnakeLadder.java

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -83,30 +83,7 @@ public class SnakeLadder {
             System.out.println("player B has won the game");
         int sum = 0, position = 0, options, diceRoll = 0;
 
-        while (sum < 101) {
-
-            options = rnum.nextInt(3);
-            diceRoll = rnum.nextInt(6) + 1;
-
-            switch (options) {
-
-                case 0:
-                    position = 0;
-                    break;
-                case 1:
-                    position = -diceRoll;
-                    break;
-                case 2:
-                    position = diceRoll;
-                    break;
-
-            }
-            sum += position;
-
-            if (sum <= 0)
-                sum = 0;
-        }
-        System.out.println("Player has reached winning position ");
+  
 
 
     }
